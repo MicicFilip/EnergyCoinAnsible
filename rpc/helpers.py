8215c772acf93711e9b1bd85d50d8d006338f1fb
MicicFilip <micic.filip@gmail.com>
2017-10-24 18:47:04 +0200

    Some logical stuff added

--- rpc/helpers.py
+++ rpc/helpers.py
@@ -8,14 +8,3 @@ def get_rpc_connection():
                                                            'rpcenergypassword',
                                                            22706))
 
-
-def get_block_count():
-    rpc_connection = get_rpc_connection()
-
-    get_number_of_blocks = rpc_connection.getblockcount()
-
-    # return get_number_of_blocks
-    print get_number_of_blocks
-
-if __name__ == "__main__":
-    get_block_count()
